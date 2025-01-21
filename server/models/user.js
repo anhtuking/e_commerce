fc133d674a248b160b1d3e1cc77d7a603cfe471f
Anh Tu <anhtu@example.comple.com>
2025-01-21 16:11:59 +0700

    hash password & handler error

---
+++ server/models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require('bcryptjs');
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Address",
+      },
+    ],
+    wishlist: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    passwordChangedAt: {
+      type: String,
+    },
+    passwordResetToken: {
+      type: String,
+    },
+    passwordResetExpires: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Method pre để Hash password before khi save 
+userSchema.pre('save', async function (next) {
+  // Only hash the password khi nó đã Modified (or is new)
+  if (!this.isModified('password')){
+    return next();
+  }
+  const salt = bcrypt.genSaltSync(10)
+  this.password = await bcrypt.hash(this.password, salt)
+});
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+const dbConnect = require('./config/db_connect.js');
+const initRoutes = require('./routes/index.js');
 
 const app = express();
 const port = process.env.PORT || 8888;
-app.use(express.json());
-app.use(express.urlencoded({ express: true }));
 
-app.use("/", (req, res) => {
-  res.json("SERVER ON!");
-});
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dbConnect();
+
+initRoutes(app);
 
 app.listen(port, () => {
   console.log("Server running on the port: " + port);
