9bfe0b9ba2bd5f8674a8be9b7736cca2006557b6
Anh Tu <anhtu@example.comple.com>
2025-02-07 16:24:26 +0700

    change password

---
+++ server/ultils/sendMail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer')
+const asyncHandler = require('express-async-handler')
+
+const sendMail = asyncHandler(async ({ email, html }) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_NAME, // generated ethereal user
+            pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"E - Commerce 👻" <no-reply@tmdt.com>',
+        to: email,
+        subject: "Forgot Password",
+        html: html,
+      });
+
+    return info
+})
+
+module.exports = sendMail
