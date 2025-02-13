5c4e337e79bcd49cb98e110c958cc05c6a39c782
Anh Tu <anhtu@example.comple.com>
2025-02-13 16:39:42 +0700

    API Like & Dislike Blog

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -29,9 +29,83 @@ const updateBlog = asyncHandler(async (req, res) => {
     })
 });
 
+const deleteBlog = asyncHandler(async (req, res) => {
+    const {bid} = req.params
+    const response = await Blog.findByIdAndDelete(bid, req.body, {new: true})
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedBlog: response ? response : 'Cannot delete Blog'
+    })
+});
+
+// LIKE & DISLIKE the Blog
+const likeBlog = asyncHandler(async (req, res) => {
+    const {id} = req.user       // lấy id của người like 
+    const {bid} = req.params      // lấy bid để xác định blog được like
+    if (!bid) throw new Error('Missing value')
+    const blog = await Blog.findById(bid)
+    // check xem user có dislike hay kh?
+    const alreadyDisliked = blog?.dislikes?.find(el => el.toString() === id)
+    if (alreadyDisliked){
+        const response = await Blog.findByIdAndUpdate(bid, {$pull: {dislikes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        }) 
+    }
+    // check xem user có like trước đó hay kh?
+    const isLiked = blog?.likes?.find(el => el.toString() === id)
+    if (isLiked){
+        const response = await Blog.findByIdAndUpdate(bid, {$pull: {likes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        }) 
+    } else{
+        const response = await Blog.findByIdAndUpdate(bid, {$push: {likes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        })
+    }
+})
+
+const dislikeBlog = asyncHandler(async (req, res) => {
+    const {id} = req.user       // lấy id của người dislike 
+    const {bid} = req.params      // lấy bid để xác định blog được dislike
+    if (!bid) throw new Error('Missing value')
+    const blog = await Blog.findById(bid)
+    // check xem user có dislike hay kh?
+    const alreadyLiked = blog?.likes?.find(el => el.toString() === id)
+    if (alreadyLiked){
+        const response = await Blog.findByIdAndUpdate(bid, {$pull: {likes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        }) 
+    }
+    // check xem user có like trước đó hay kh?
+    const isDisliked = blog?.dislikes?.find(el => el.toString() === id)
+    if (isDisliked){
+        const response = await Blog.findByIdAndUpdate(bid, {$pull: {dislikes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        }) 
+    } else{
+        const response = await Blog.findByIdAndUpdate(bid, {$push: {dislikes: id}}, {new: true})
+        return res.status(200).json({
+            success: response ? true : false,
+            result: response
+        })
+    }
+})
 
 module.exports = {
     createNewBlog,
     getAllBlogs,
     updateBlog,
+    deleteBlog,
+    likeBlog,
+    dislikeBlog
 }
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -4,7 +4,9 @@ const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 
 router.post('/', [verifyAccessToken, isAdmin], ctrls.createNewBlog)
 router.get('/', ctrls.getAllBlogs)
-
-router.put('/:bid', [verifyAccessToken, isAdmin], ctrls.updateBlog)
+router.delete('/delete/:bid', [verifyAccessToken, isAdmin], ctrls.deleteBlog)
+router.put('/update/:bid', [verifyAccessToken, isAdmin], ctrls.updateBlog)
+router.put('/like/:bid', [verifyAccessToken], ctrls.likeBlog)
+router.put('/dislike/:bid', [verifyAccessToken], ctrls.dislikeBlog)
 
 module.exports = router
