63a7d6d5bd9b8c5b23a7d7053ce0a2bbd8d028b0
Anh Tu <anhtu@example.comple.com>
2025-02-26 10:12:38 +0700

    data

---
+++ data/cate_brand.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        cate: 'Smartphone',
+        brand: ['Apple', 'Samsung', 'Xiaomi', 'Oppo', 'Realme', 'Vivo']
+    },
+    {
+        cate: 'Tablet',
+        brand: ['iPad', 'Samsung', 'Huawei', 'Xiaomi', 'Lenovo']
+    },
+    {
+        cate: 'Laptop',
+        brand: ['Asus', 'Dell', 'Acer', 'Lenovo', 'HP', 'Mac', 'MSI']
+    },
+    {
+        cate: 'Accessories',
+        brand: ['Memory card', 'Headphone', 'Bluetooth', 'Keyboard', 'Mouse', 'Case']
+    },
+    {
+        cate: 'Television',
+        brand: ['Sony', 'Samsung', 'Xiaomi', 'LG', 'TCL', 'Aqua']
+    },
+    {
+        cate: 'Printer',
+        brand: ['HP', 'Brother', 'HPRT']
+    },
+]
+
+module.exports = data
