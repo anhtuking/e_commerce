c71cda3c85ab2b13f4bfc7023e9c6a81139f9f20
Anh Tu <anhtu@example.comple.com>
2025-02-27 21:33:30 +0700

    Build base-reactJS

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    fontFamily: {
+      main: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      width: {
+        main: "1220px",
+      },
+      colors: {
+        main: "#ee3131",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
