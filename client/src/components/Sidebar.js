a0905c4737776ec756e36dc8d9d82d5ca4d883dd
Anh Tu <anhtu@example.comple.com>
2025-03-01 16:12:04 +0700

    header & navigation

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect,useState } from "react";
+import { apiGetCategories } from "../api/app";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    )
-}
+  const [categories, setCategories] =  useState(null)
+  const fecthCategories = async () => {
+    const response = await apiGetCategories();
+    if (response.success) setCategories(response.productCategories)
+  };
+  useEffect(() => {
+    fecthCategories();
+  }, [])
+//   console.log(categories);
+  return <div>Sidebar</div>;
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ client/src/utils/icons.js
@@ -0,0 +1,15 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { IoBagHandle } from "react-icons/io5";
+import { FaUserAlt } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+
+const icons = {
+    FaPhoneAlt,
+    MdEmail,
+    IoBagHandle,
+    FaUserAlt,
+    AiFillHome
+}
+
+export default icons
