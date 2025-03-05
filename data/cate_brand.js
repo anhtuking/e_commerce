66a07c8cfe5978067f588ca92cec6752d97aff16
Anh Tu <anhtu@example.comple.com>
2025-03-05 12:49:35 +0700

    update data

--- data/cate_brand.js
+++ data/cate_brand.js
@@ -1,28 +1,51 @@
 const data = [
-    {
-        cate: 'Smartphone',
-        brand: ['Apple', 'Samsung', 'Xiaomi', 'Oppo', 'Realme', 'Vivo']
-    },
-    {
-        cate: 'Tablet',
-        brand: ['iPad', 'Samsung', 'Huawei', 'Xiaomi', 'Lenovo']
-    },
-    {
-        cate: 'Laptop',
-        brand: ['Asus', 'Dell', 'Acer', 'Lenovo', 'HP', 'Mac', 'MSI']
-    },
-    {
-        cate: 'Accessories',
-        brand: ['Memory card', 'Headphone', 'Bluetooth', 'Keyboard', 'Mouse', 'Case']
-    },
-    {
-        cate: 'Television',
-        brand: ['Sony', 'Samsung', 'Xiaomi', 'LG', 'TCL', 'Aqua']
-    },
-    {
-        cate: 'Printer',
-        brand: ['HP', 'Brother', 'HPRT']
-    },
-]
+  {
+    cate: "Smartphone",
+    brand: ["Apple", "Samsung", "Xiaomi", "Oppo", "Realme", "Vivo"],
+    image: 'https://www.top10mobileshop.com/images/top10mobiles.com/thumbnail/product/2024/08/218567656202408060913.jpg'
+  },
+  {
+    cate: "Tablet",
+    brand: ["iPad", "Samsung", "Huawei", "Xiaomi", "Lenovo"],
+    image: 'https://dt24h.com/wp-content/uploads/2024/08/Lenovo-Xiaoxin-Pad-Pro-12.7-inch-2025-Paperlike-Screen-1.jpg'
+  },
+  {
+    cate: "Laptop",
+    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Mac", "MSI"],
+    image: 'https://hanoicomputercdn.com/media/product/84654_laptop_hp_15_fd1058tu_9z2x6pa_ultra_7_155u_16gb_ram_1tb_ssd_15__5_.jpg'
+  },
+  {
+    cate: "Accessories",
+    brand: [
+      "Memory card",
+      "Headphone",
+      "Bluetooth",
+      "Keyboard",
+      "Mouse",
+      "Case",
+    ],
+    image: 'https://nvs.tn-cdn.net/2023/11/tai-nghe-xiberia-q4.jpg'
+  },
+  {
+    cate: "Television",
+    brand: ["Sony", "Samsung", "Xiaomi", "LG", "TCL", "Aqua"],
+    image: 'https://dinapalagroup.lk/wp-content/uploads/2024/08/65PUT7129.webp'
+  },
+  {
+    cate: "Printer",
+    brand: ["HP", "Brother", "HPRT", "Lenovo", "Haier"],
+    image: 'https://product.hstatic.net/200000379859/product/may_in_dai_thanh__17__e15978d650f84baf80126e2070c8a560_1024x1024.png'
+  },
+  {
+    cate: "Speaker",
+    brand: [],
+    image: 'https://www.kantoaudio.com/wp-content/uploads/passive-speakers-category-panel.png'
+  },
+  {
+    cate: "Camera",
+    brand: [],
+    image: 'https://cameramytho.com/wp-content/uploads/2023/04/Imou-F22P.png'
+  },
+];
 
-module.exports = data
+module.exports = data;
